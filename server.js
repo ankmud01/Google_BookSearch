e7153f53753a4f038888623b0db1107b47419909
Rebel Soul <mudvari.ankit@gmail.com>
2020-07-21 16:34:53 -0400

    added a test route..

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// ADD ROUTES FOR BOTH API AND VIEW
+//Test my connection is working
+app.get("/api/config", (req,res) =>{
+    res.json({
+        error: false,
+        data: null,
+        message: "Success",
+    })
+});
+
+// TODO MONGOOSE CONNECTION
 
 
 app.listen(PORT, function(){
